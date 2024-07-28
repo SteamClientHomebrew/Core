b4daae28469518dfa28a775ccefdcd728aaaa670
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2024-07-28 18:39:50 +0000

    refactor: use DialogButton and caution steam icon

--- .millennium/Dist/index.js
+++ .millennium/Dist/index.js
@@ -206,7 +206,7 @@ var millennium_main = (function (exports, React, ReactDOM) {
     const DialogButton = DialogButtonSecondary;
 
     // Button isn't exported, so call DialogButton to grab it
-    const Button = DialogButton?.render({}).type;
+    DialogButton?.render({}).type;
 
     /**
      * Create a Regular Expression to search for a React component that uses certain props in order.
@@ -1485,14 +1485,10 @@ var millennium_main = (function (exports, React, ReactDOM) {
         return (window.SP_REACT.createElement("div", { className: "__up-to-date-container", style: {
                 display: "flex", flexDirection: "column", alignItems: "center", height: "100%", justifyContent: "center"
             } },
-            window.SP_REACT.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", xmlnsXlink: "http://www.w3.org/1999/xlink", version: "1.1", width: 25, height: 25, viewBox: "0 0 256 256", xmlSpace: "preserve" },
-                window.SP_REACT.createElement("defs", null),
-                window.SP_REACT.createElement("g", { style: { stroke: 'none', strokeWidth: 0, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'none', fillRule: 'nonzero', opacity: 1 }, transform: "translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" },
-                    window.SP_REACT.createElement("path", { d: "M 11 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 c -4.295 -4.296 -4.295 -11.261 0 -15.557 l 68 -68 c 4.297 -4.296 11.26 -4.296 15.557 0 c 4.296 4.296 4.296 11.261 0 15.557 l -68 68 C 16.63 88.926 13.815 90 11 90 z", style: { stroke: 'none', strokeWidth: 1, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'rgb(214,0,0)', fillRule: 'nonzero', opacity: 1 }, transform: " matrix(1 0 0 1 0 0) ", strokeLinecap: "round" }),
-                    window.SP_REACT.createElement("path", { d: "M 79 90 c -2.815 0 -5.63 -1.074 -7.778 -3.222 l -68 -68 c -4.295 -4.296 -4.295 -11.261 0 -15.557 c 4.296 -4.296 11.261 -4.296 15.557 0 l 68 68 c 4.296 4.296 4.296 11.261 0 15.557 C 84.63 88.926 81.815 90 79 90 z", style: { stroke: 'none', strokeWidth: 1, strokeDasharray: 'none', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 10, fill: 'rgb(214,0,0)', fillRule: 'nonzero', opacity: 1 }, transform: " matrix(1 0 0 1 0 0) ", strokeLinecap: "round" }))),
+            window.SP_REACT.createElement(IconsModule.Caution, { width: "64" }),
             window.SP_REACT.createElement("div", { className: "__up-to-date-header", style: { marginTop: "20px", color: "white", fontWeight: "500", fontSize: "15px" } }, "Failed to connect to Millennium!"),
             window.SP_REACT.createElement("p", { style: { fontSize: "12px", color: "grey", textAlign: "center", maxWidth: "76%" } }, "This issue isn't network related, you're most likely missing a file millennium needs, or are experiencing an unexpected bug."),
-            window.SP_REACT.createElement(Button, { onClick: () => SteamClient.System.OpenLocalDirectoryInSystemExplorer("ext\\data\\logs\\"), style: { marginTop: "20px" } }, "Open Logs Folder")));
+            window.SP_REACT.createElement(DialogButton, { onClick: () => SteamClient.System.OpenLocalDirectoryInSystemExplorer("ext\\data\\logs\\"), style: { width: "50%", marginTop: "20px" } }, "Open Logs Folder")));
     };
 
     const containerClasses = [
