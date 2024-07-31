82b4ff4bc7de41b8ab5e842b83b9d9f65f139ef6
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-07-31 14:11:07 -0300

    fix: Fix 'Open Logs Folder' on linux.

--- src/custom_components/ConnectionFailed.tsx
+++ src/custom_components/ConnectionFailed.tsx
@@ -1,6 +1,12 @@
-import { DialogButton, IconsModule } from "@millennium/ui";
+import { DialogButton, IconsModule, pluginSelf } from "@millennium/ui";
 
 export const ConnectionFailed = () => {
+
+    const OpenLogsFolder = () => {
+        const logsPath = [pluginSelf.steamPath, "ext", "data", "logs"].join("/");
+        SteamClient.System.OpenLocalDirectoryInSystemExplorer(logsPath);
+    }
+
     return (
         <div className="__up-to-date-container" style={{
             display: "flex", flexDirection: "column", alignItems: "center", height: "100%", justifyContent: "center"
@@ -10,7 +16,7 @@ export const ConnectionFailed = () => {
             <div className="__up-to-date-header" style={{marginTop: "20px", color: "white", fontWeight: "500", fontSize: "15px"}}>Failed to connect to Millennium!</div>
             <p style={{ fontSize: "12px", color: "grey", textAlign: "center", maxWidth: "76%" }}>This issue isn't network related, you're most likely missing a file millennium needs, or are experiencing an unexpected bug.</p>
 
-            <DialogButton onClick={() => SteamClient.System.OpenLocalDirectoryInSystemExplorer("ext\\data\\logs\\")} style={{width: "50%", marginTop: "20px"}}>Open Logs Folder</DialogButton>
+            <DialogButton onClick={OpenLogsFolder} style={{width: "50%", marginTop: "20px"}}>Open Logs Folder</DialogButton>
         </div>
     );
 }
